9bd503e45ba2b7792dc4c43dc977765804d14da9
ori-volfo <ori.volfo@gmail.com>
2018-01-03 13:29:53 +0200

    building routes

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -4,11 +4,16 @@ import {Routes, RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
 import {AboutComponent} from './about/about.component';
 import {ProjectsComponent} from './projects/projects.component';
+import {TaskManagerComponent} from './projects/task-manager/task-manager.component';
 
 export const router: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full'},
-  { path: 'projects', component: ProjectsComponent},
   { path: 'about', component: AboutComponent},
+  { path: 'projects', component: ProjectsComponent,
+    children: [
+        { path: 'task-manager', component: TaskManagerComponent}
+      ]
+  },
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
